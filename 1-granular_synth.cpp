01698c62fc8914909248c08bbe47bb83cebec876
Jack Kilgore <jkilgore@ucsb.edu>
2018-11-20 18:07:02 -0800

    stereo attempting

--- 1-granular_synth.cpp
+++ 1-granular_synth.cpp
@@ -39,16 +39,27 @@ struct Granulator {
       cout << "We could not read " << fileName << "1" << endl;
       exit(1);
     }
-    if(soundFile.channels() != 1){
-      cout << fileName << " is not a mono file." << endl;
+    if(soundFile.channels() >= 2){
+      cout << fileName << " is not a mono or stereo file" << endl;
       exit(1);
     }
 
-    Array *a = new Array();
-    a->size = soundFile.frames();
-    a->data = new float[a->size];
-    soundFile.read(a->data, a->size);
-    this->soundClip.push_back(a);
+    if(soundFile.channels() == 1){
+      Array *a = new Array();
+      a->size = soundFile.frames();
+      a->data = new float[a->size];
+      soundFile.read(a->data, a->size);
+      this->soundClip.push_back(a);
+    }
+
+    if(soundFile.channels() == 2){
+      Array *stereo = new Array();
+      stereo->size = soundFile.frames()*soundFile.channels();
+      stereo->data = new float[stereo->size];
+      soundFile.read(stereo->data, stereo->size);
+      this->soundClip.push_back(stereo);
+    }
+    
 
     soundFile.close();
   }
@@ -143,20 +154,53 @@ struct Granulator {
 };
 
 struct MyApp : public App {
+   bool show_gui = true;
+   float background = 0.21;
    Granulator granulator;
    Recv server;
 
   void onCreate() override {
+     initIMGUI();
      server.open(9020,"localhost",0.05);
      server.handler(*this);
      server.start();
+
+     granulator.load("0.wav");
+     granulator.load("1.wav");
+     granulator.load("2.wav");
+     granulator.load("3.wav");
+     granulator.load("4.wav");
+     //granulator.load("improv_stereo2.wav");
   }
 
-  // called once per graphics frame ~30Hz
-  //
+  void onAnimate(double dt) override {
+    // pass show_gui for use_input param to turn off interactions
+    // when not showing gui
+    beginIMGUI_minimal(show_gui);
+    navControl().active(!imgui_is_using_input());
+  }
+
   void onDraw(Graphics& g) override {
-    g.clear(0);
-    //
+    g.clear(background);
+    ImGui::Text("Active Grains: %3d", granulator.activeGrainCount);
+    ImGui::SliderFloat("Background", &background, 0, 1);
+
+    ImGui::SliderInt("Sound Clip", &granulator.whichClip, 0, 5);
+    ImGui::SliderFloat("Start Position", &granulator.startPosition, 0, 1);
+    ImGui::SliderFloat("Playback Rate", &granulator.playbackRate, -1, 1);
+
+    static float volume = -7;
+    ImGui::SliderFloat("Loudness", &volume, -42, 0);
+    granulator.amplitudePeak = dbtoa(volume);
+
+    ImGui::SliderFloat("Envelop Parameter", &granulator.peakPosition, 0, 1);
+    ImGui::SliderFloat("Grain Duration", &granulator.grainDuration, 0.001, 0.5);
+
+    static float midi = 10;
+    ImGui::SliderFloat("Birth Frequency", &midi, -16, 85);
+    granulator.grainBirth.frequency(mtof(midi));
+
+    endIMGUI_minimal(show_gui);
   }
 
   // called once per audio frame ~86Hz (SAMPLE_RATE / BLOCK_SIZE)
@@ -164,9 +208,15 @@ struct MyApp : public App {
   void onSound(AudioIOData& io) override {
     while (io()) {
       // this inner code block runs once per sample
-      float s = 0;
+      float s = granulator();
       io.out(0) = s;
       io.out(1) = s;
+    }
+  }
+
+  void onKeyDown(const Keyboard& k) override {
+    if (k.key() == 'g') {
+      show_gui = !show_gui;
     }
   }
 
@@ -198,6 +248,8 @@ struct MyApp : public App {
     }
 
   }
+
+  void onExit() override { shutdownIMGUI(); }
 };
 
 
